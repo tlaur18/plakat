2dc4eefde83950851e19f665643120ccbb3ead0d
Thomas Steenfeldt <tlaur18@student.sdu.dk>
2021-06-03 14:59:38 +0200

    Requesting favicon from Github

--- pythonserver.py
+++ pythonserver.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
 import urllib.request
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -9,6 +10,12 @@ def hello():
     # return render_template("index.html")
     return urllib.request.urlopen('https://raw.githubusercontent.com/tlaur18/plakat/main/templates/index.html').read()
 
+
+@app.route('/favicon.ico')
+def favicon():
+    # return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return urllib.request.urlopen('https://raw.githubusercontent.com/tlaur18/plakat/main/static/favicon.ico').read()
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5005)
